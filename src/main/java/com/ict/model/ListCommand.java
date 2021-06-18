dd7ac96535284586f898ea492059197bb18f7eca
nohssam <nohssam73@gmail.net>
2021-06-18 09:34:24 +0900

    ans_write

--- src/main/java/com/ict/model/ListCommand.java
+++ src/main/java/com/ict/model/ListCommand.java
@@ -26,7 +26,7 @@ public class ListCommand implements Command{
 		}
 		// 3. 현재 페이지 구하기 
 		String cPage = request.getParameter("cPage");
-		if(cPage==null) cPage = "1";
+		if(cPage=="") cPage = "1";
 		pvo.setNowPage(Integer.parseInt(cPage));
 		
 		// 4. 시작번호, 끝번호 구하기 
